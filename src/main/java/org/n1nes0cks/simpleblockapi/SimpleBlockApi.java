7a84ac120dc052764a4643220e90c8c393112279
n1NeS0cKs <assasinnsk@mail.ru>
2025-01-29 20:36:27 +0700

    create

---
+++ src/main/java/org/n1nes0cks/simpleblockapi/SimpleBlockApi.java
@@ -0,0 +1,17 @@
+package org.n1nes0cks.simpleblockapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleBlockApi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
